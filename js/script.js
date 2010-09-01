c059b31dc2e2b68cc69a4f204429d51063be8889
Mister Gingle <matthieu.aussaguel@gmail.com>
2010-09-01 22:41:15 +1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/*
+ * jQuery Word Count plug-in 0.1
+ *
+ * Copyright (c) 2010 - Matthieu Aussaguel
+ *
+ */
+
+$(document).ready(function() {
+	$('#textarea-main').focus();	
+	$('#textarea-main').wordcount({counter: 'wordcount'});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
